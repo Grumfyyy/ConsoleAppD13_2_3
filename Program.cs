d7900bb11e3781e257ec33f81331eb6c064f5d98
Josef <swadijosef@gmail.com>
2023-03-01 20:55:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppD13_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
